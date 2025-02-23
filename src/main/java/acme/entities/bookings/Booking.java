b154b3a1f84a7094974ced36748d6c351faa78b0
josmorgue3 <josmorgue3@alum.us.es>
2025-02-23 17:10:51 +0100

    Primera version Task04 Datos de las reservas

---
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -0,0 +1,73 @@
+
+package acme.entities.bookings;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.entities.customers.Customer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking<Costumer> extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Column(unique = true)
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				purchaseMoment;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				price;
+
+	@Optional
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				lastNibble;
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@OneToOne(optional = true)
+	private Customer			custumer;
+
+	//	@Mandatory
+	//	@Valid
+	//	@OneToOne(optional = true)
+	//	private Flight				flight;
+
+}
