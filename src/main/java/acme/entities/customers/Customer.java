5028fd01b2979b35e8fb248e666eeaa5a65e322a
josmorgue3 <josmorgue3@alum.us.es>
2025-02-23 13:14:07 +0100

    Primera version Task03-I Datos de los clientes

---
+++ src/main/java/acme/entities/customers/Customer.java
@@ -0,0 +1,57 @@
+
+package acme.entities.customers;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				id;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				physicalAddress;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				city;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				country;
+
+	@Optional
+	@ValidNumber(max = 500000)
+	@Automapped
+	private Integer				earnedPoints;
+
+}
