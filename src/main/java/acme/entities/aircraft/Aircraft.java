de36247f50c918075e40312872386cab30bb4b13
pabalcber <pabloalcantarabernal@gmail.com>
2025-03-03 09:25:02 +0100

    Aircraft: Primera versión

---
+++ src/main/java/acme/entities/aircraft/Aircraft.java
@@ -0,0 +1,55 @@
+
+package acme.entities.aircraft;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Aircraft extends AbstractEntity {
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				model;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Column(unique = true)
+	private String				registrationNumer;
+
+	@Mandatory
+	@ValidNumber
+	@Automapped
+	private int					capacity;
+
+	@Mandatory
+	@ValidNumber(min = 2, max = 50)
+	@Automapped
+	private int					cargoWeight;
+
+	@Mandatory
+	@Automapped
+	private boolean				status;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				details;
+
+}
