d41867217be0750fcf5f7b5091075eb8ed1ddb9c
pabalcber <pabloalcantarabernal@gmail.com>
2025-03-07 15:43:17 +0100

    Fix: Relación Aircraft pertence a Airline

--- src/main/java/acme/entities/aircraft/Aircraft.java
+++ src/main/java/acme/entities/aircraft/Aircraft.java
@@ -3,6 +3,8 @@ package acme.entities.aircraft;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -10,6 +12,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -52,4 +55,10 @@ public class Aircraft extends AbstractEntity {
 	@Automapped
 	private String				details;
 
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 }
