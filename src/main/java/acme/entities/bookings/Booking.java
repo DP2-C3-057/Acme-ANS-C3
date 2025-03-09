dcf55e16bbb1e02e5bf91908bc16b0db9cef6fff
josmorgue3 <josmorgue3@alum.us.es>
2025-03-09 12:58:20 +0100

    Arregladas las entidades Individuales D02

--- src/main/java/acme/entities/bookings/Booking.java
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -18,6 +18,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.entities.flights.Flight;
 import acme.realms.Customer;
 import lombok.Getter;
 import lombok.Setter;
@@ -54,9 +55,9 @@ public class Booking extends AbstractEntity {
 	private Money				price;
 
 	@Optional
-	@ValidMoment(past = true)
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date				lastNibble;
+	@ValidString(pattern = "^[0-9]{4}$")
+	@Automapped
+	private String				lastCardNibble;
 
 	// Relationships ----------------------------------------------------------
 
@@ -65,9 +66,9 @@ public class Booking extends AbstractEntity {
 	@ManyToOne
 	private Customer			customer;
 
-	//	@Mandatory
-	//	@Valid
-	//	@ManyToOne
-	//	private Flight				flight;
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Flight				flight;
 
 }
