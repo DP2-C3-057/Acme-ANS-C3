2afa7a34a42fff927302557a4c8237e4b19a74ed
pabalcber <pabloalcantarabernal@gmail.com>
2025-03-19 11:09:13 +0100

    S1 D03: List flight primera versión

---
+++ src/main/java/acme/features/manager/flight/ManagerFlightController.java
@@ -0,0 +1,29 @@
+
+package acme.features.manager.flight;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.flights.Flight;
+import acme.realms.managers.Manager;
+
+@GuiController
+public class ManagerFlightController extends AbstractGuiController<Manager, Flight> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ManagerFlightListService listService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+	}
+
+}
--- src/main/resources/WEB-INF/views/fragments/menu.jsp
+++ src/main/resources/WEB-INF/views/fragments/menu.jsp
@@ -34,6 +34,10 @@
 			<acme:menu-suboption code="master.menu.administrator.shut-system-down" action="/administrator/system/shut-down"/>
 		</acme:menu-option>
 
+		<acme:menu-option code="master.menu.manager" access="hasRealm('Manager')">
+			<acme:menu-suboption code="master.menu.manager.list-my-flights" action="/manager/flight/list"/>
+		</acme:menu-option>
+
 		<acme:menu-option code="master.menu.provider" access="hasRealm('Provider')">
 			<acme:menu-suboption code="master.menu.provider.favourite-link" action="http://www.example.com/"/>
 		</acme:menu-option>
