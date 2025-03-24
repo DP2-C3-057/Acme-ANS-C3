7f0a79f80ad253318a065c409cdf3e9f8e028c62
pabalcber <pabloalcantarabernal@gmail.com>
2025-03-24 19:54:21 +0100

    S01 D03: FlightPublish primera versión

--- src/main/java/acme/features/manager/flight/ManagerFlightController.java
+++ src/main/java/acme/features/manager/flight/ManagerFlightController.java
@@ -30,6 +30,9 @@ public class ManagerFlightController extends AbstractGuiController<Manager, Flig
 	@Autowired
 	private ManagerFlightDeleteService	deleteService;
 
+	@Autowired
+	private ManagerFlightPublishService	publishService;
+
 	// Constructors -----------------------------------------------------------
 
 
@@ -40,6 +43,8 @@ public class ManagerFlightController extends AbstractGuiController<Manager, Flig
 		super.addBasicCommand("create", this.createService);
 		super.addBasicCommand("update", this.updateService);
 		super.addBasicCommand("delete", this.deleteService);
+
+		super.addCustomCommand("publish", "update", this.publishService);
 	}
 
 }
