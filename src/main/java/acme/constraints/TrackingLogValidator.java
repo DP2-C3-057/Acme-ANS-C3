22cee32eed9999ffcb5d5d562d7bb92858417026
ALEXIS MOLINS <alexismolinstic@gmail.com>
2025-03-29 17:32:52 +0100

    preactualizacion

--- src/main/java/acme/constraints/TrackingLogValidator.java
+++ src/main/java/acme/constraints/TrackingLogValidator.java
@@ -1,6 +1,49 @@
+
 package acme.constraints;
 
+import javax.validation.ConstraintValidatorContext;
 
-public class TrackingLogValidator {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.validation.AbstractValidator;
+import acme.entities.trackingLogs.TrackingLog;
+import acme.entities.trackingLogs.TrackingLogRepository;
+import acme.entities.trackingLogs.TrackingLogStatus;
+
+public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, TrackingLog> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private TrackingLogRepository repository;
+
+	// ConstraintValidator interface ------------------------------------------
+
+
+	@Override
+	protected void initialise(final ValidTrackingLog annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final TrackingLog trackingLog, final ConstraintValidatorContext context) {
+		assert context != null;
+
+		boolean result;
+
+		if (trackingLog == null)
+			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else {
+			boolean correctPercentaje = true;
+			if (trackingLog.getIndicator() == TrackingLogStatus.ACCEPTED || trackingLog.getIndicator() == TrackingLogStatus.REJECTED)
+				if (trackingLog.getResolutionPercentage() != 100)
+					correctPercentaje = false;
+
+			super.state(context, correctPercentaje, "*", "acme.validation.trackingLog.correctPercentaje.message");
+		}
+
+		result = !super.hasErrors(context);
+
+		return result;
+	}
 }
