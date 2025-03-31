d62dd6807370cf8e4a8cb416b42fe7568e9cfe2d
RukisRo <rociolopez08@gmail.com>
2025-03-31 13:01:36 +0200

    Task08: añadido listar y mostrar

---
+++ src/main/java/acme/features/flightCrewMember/flightAssignment/FlightCrewMemberAssignmentPlannedListService.java
@@ -0,0 +1,46 @@
+
+package acme.features.flightCrewMember.flightAssignment;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.flightAssignments.FlightAssignment;
+import acme.entities.legs.LegStatus;
+import acme.realms.flightCrewMembers.FlightCrewMember;
+
+@GuiService
+public class FlightCrewMemberAssignmentPlannedListService extends AbstractGuiService<FlightCrewMember, FlightAssignment> {
+
+	@Autowired
+	private FlightCrewMemberAssignmentRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		int memberId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		Collection<LegStatus> statuses = List.of(LegStatus.ON_TIME, LegStatus.DELAYED);
+		Collection<FlightAssignment> assignments = this.repository.assignmentsWithPlannedLegs(statuses, memberId);
+		super.getBuffer().addData(assignments);
+	}
+
+	@Override
+	public void unbind(final FlightAssignment assignment) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(assignment, "duty", "moment", "currentStatus");
+		super.addPayload(dataset, assignment, "remarks", "draftMode", "flightCrewMember.identity.fullName", "leg.status");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
