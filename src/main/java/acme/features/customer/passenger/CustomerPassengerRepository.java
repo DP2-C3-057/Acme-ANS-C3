ea54664feef1376b32b35f99ac3ada6c24001212
josmorgue3 <josmorgue3@alum.us.es>
2025-04-03 13:05:59 +0200

    Creada la lista de todos los pasajeros del customer

--- src/main/java/acme/features/customer/passenger/CustomerPassengerRepository.java
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerRepository.java
@@ -3,6 +3,7 @@ package acme.features.customer.passenger;
 
 import java.util.Collection;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,7 +16,7 @@ import acme.realms.customers.Customer;
 @Repository
 public interface CustomerPassengerRepository extends AbstractRepository {
 
-	@Query("SELECT br.passenger FROM BookingRecord br WHERE br.booking.customer.id = :customerId")
+	@Query("SELECT p FROM Passenger p WHERE p.customer.id = :customerId")
 	Collection<Passenger> findPassengersByCustomerId(@Param("customerId") int customerId);
 
 	@Query("SELECT br.passenger FROM BookingRecord br WHERE br.booking.id = :bookingId")
@@ -30,7 +31,11 @@ public interface CustomerPassengerRepository extends AbstractRepository {
 	@Query("select c from Customer c where c.id = :masterId")
 	Customer findCustomerById(int masterId);
 
-	@Query("SELECT br.booking.customer FROM BookingRecord br WHERE br.passenger.id = :passengerId")
+	@Query("SELECT p.customer FROM Passenger p WHERE p.id = :passengerId")
 	Customer findCustomerByPassengerId(@Param("passengerId") int passengerId);
+
+	@Modifying
+	@Query("DELETE FROM BookingRecord br WHERE br.passenger.id = :passengerId")
+	void deleteBookingRecordsByPassengerId(@Param("passengerId") int passengerId);
 
 }
