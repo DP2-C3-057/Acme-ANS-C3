c3e5daa33d844a574681d839bc1e70853ceabf34
ALEXIS MOLINS <alexismolinstic@gmail.com>
2025-04-18 15:47:22 +0100

    trackinglog done

--- src/main/java/acme/constraints/TrackingLogValidator.java
+++ src/main/java/acme/constraints/TrackingLogValidator.java
@@ -1,11 +1,15 @@
 
 package acme.constraints;
 
+import java.util.Comparator;
+import java.util.List;
+
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.validation.AbstractValidator;
+import acme.entities.claims.Claim;
 import acme.entities.trackingLogs.TrackingLog;
 import acme.entities.trackingLogs.TrackingLogRepository;
 import acme.entities.trackingLogs.TrackingLogStatus;
@@ -49,6 +53,30 @@ public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, Tr
 						correctResolution = false;
 
 				super.state(context, correctResolution, "*", "acme.validation.trackingLog.correctResolution.message");
+			}
+			{
+				boolean correctPercentajeClaim = true;
+				List<Claim> claims = this.repository.findAllClaimsWithTrackingLogs();
+				for (Claim claim : claims) {
+					List<TrackingLog> trackingLogsClaim = this.repository.findTrackingLogByClaimId(claim);
+					trackingLogsClaim.sort(Comparator.comparing(TrackingLog::getLastUpdateMoment));
+					for (int i = 0; i < trackingLogsClaim.size() - 1; i++) {
+						TrackingLog actual = trackingLogsClaim.get(i);
+						TrackingLog siguiente = trackingLogsClaim.get(i + 1);
+						if (siguiente.getLastUpdateMoment().after(actual.getLastUpdateMoment()) && siguiente.getResolutionPercentage() <= actual.getResolutionPercentage()) {
+							correctPercentajeClaim = false;
+							super.state(context, correctPercentajeClaim, "*", "acme.validation.trackingLog.correctPercentajeClaim.message");
+							break;
+						}
+					}
+				}
+			}
+			{
+				boolean correctAgent = true;
+				if (!trackingLog.getAssistanceAgent().equals(this.repository.findAgentByClaim(trackingLog.getClaim())))
+					correctAgent = false;
+
+				super.state(context, correctAgent, "*", "acme.validation.trackingLog.correctAgent.message");
 			}
 		}
 
--- src/main/java/acme/entities/trackingLogs/TrackingLogRepository.java
+++ src/main/java/acme/entities/trackingLogs/TrackingLogRepository.java
@@ -1,8 +1,23 @@
 
 package acme.entities.trackingLogs;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
 import acme.client.repositories.AbstractRepository;
+import acme.entities.claims.Claim;
+import acme.realms.assistanceAgents.AssistanceAgent;
 
 public interface TrackingLogRepository extends AbstractRepository {
+
+	@Query("select t from TrackingLog t where t.claim = :claimId")
+	List<TrackingLog> findTrackingLogByClaimId(Claim claimId);
+
+	@Query("select distinct t.claim from TrackingLog t")
+	List<Claim> findAllClaimsWithTrackingLogs();
+
+	@Query("select c.assistanceAgent from Claim c where c = :claim")
+	AssistanceAgent findAgentByClaim(Claim claim);
 
 }
